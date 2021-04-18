5c243f29c8ec63c0202ee85bf718b8615ba2c664
Liberto <lbianchi@gmail.com>
2021-04-19 01:39:36 +0200

    Ejercicio1

--- src/Java/Ej1.java
+++ src/Java/Ej1.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Ej1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserta un numero en la consola");
+        int num = sc.nextInt();
+        System.out.println((char)+num);
+    }
+
 }
