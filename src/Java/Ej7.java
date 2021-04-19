cadae6f02487bb9dde01010c112dde73173437f8
Liberto <lbianchi@gmail.com>
2021-04-19 02:23:50 +0200

    Ejercicios 6/7

---
+++ src/Java/Ej7.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ej7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserta un numero de productos vendidos");
+        int numProd = sc.nextInt();
+        float valorProductos = 0;
+        for (int i = 1; i < numProd+1; i++) {
+            float valorProducto=0;
+            System.out.println("Inserta el valor del producto "+i);
+            valorProducto=sc.nextFloat();
+            valorProductos += valorProducto;
+        }
+        System.out.println("El valor total es = " +valorProductos);
+    }
+//Vuelvo a tener errores con los float
+}
